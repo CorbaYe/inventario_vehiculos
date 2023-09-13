ea7d88ab814a702dc7fb1c84d91076783f62e9ab
CorbaYe <corba.ye99@gmail.com>
2023-09-13 06:48:24 -0500

    Terminando menu

--- app_inventario.java
+++ app_inventario.java
@@ -2,10 +2,17 @@ import javax.swing.JOptionPane;
 
 public class app_inventario {
     public static void main(String[] args) {
-        
+        fnt_menu(true);
     }
+
     private static void fnt_menu(boolean m){
-        String menu = JOptionPane.showInputDialog(null, "<<< MENU PRINCIPAL >>>\n1. Registrar\n2. Consultar\n3. Actaualizar\n4. Salir");
+        do{
+            int menu = Integer.parseInt(JOptionPane.showInputDialog(null, "<<< MENU PRINCIPAL >>>\n1. Registrar\n2. Consultar\n3. Actaualizar\n4. Salir"));
+
+            switch(menu){
+                case 4: m = false;
+            }
+        }while(m);
     }
 
 }
