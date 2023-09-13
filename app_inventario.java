bcd25163ecc7297df843b17ac86e4dc740aa4506
CorbaYe <corba.ye99@gmail.com>
2023-09-13 06:41:55 -0500

    opciones del menu

--- app_inventario.java
+++ app_inventario.java
@@ -1,7 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class app_inventario {
     public static void main(String[] args) {
         
     }
-    private vo
+    private static void fnt_menu(boolean m){
+        String menu = JOptionPane.showInputDialog(null, "<<< MENU PRINCIPAL >>>\n1. Registrar\n2. Consultar\n3. Actaualizar\n4. Salir");
+    }
 
 }
