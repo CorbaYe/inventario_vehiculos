2779e597794401c836270cc6d2d2e248a46973b7
CorbaYe <corba.ye99@gmail.com>
2023-09-13 07:21:58 -0500

    Terminando metodo agregar

--- app_inventario.java
+++ app_inventario.java
@@ -1,14 +1,19 @@
 import javax.swing.JOptionPane;
 
 public class app_inventario {
-
+    static cls_inventario [] inventario = new cls_inventario[50];
+    static int int_posicion = 0;
     public static void main(String[] args) {
         fnt_menu(true);
     }
 
     private static void fnt_agregar(String propietario, String placa,String fecha, int modelo, String color, String fichaTecnica){
         if(propietario.equals("") || placa.equals("") || fecha.equals("") || color.equals("") || fichaTecnica.equals("")){
-            
+            JOptionPane.showMessageDialog(null, "Debe ingresar todos los datos requeridos", "Registro", JOptionPane.ERROR_MESSAGE);
+        }else{
+            inventario[int_posicion] = new cls_inventario(propietario, placa, fecha, modelo, color, fichaTecnica);
+            int_posicion++;
+            JOptionPane.showMessageDialog(null, "Datos almacenados con éxito", "Registro", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
